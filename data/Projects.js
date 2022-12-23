5a7652dcb76c0e59596629df1e611cd7159a5287
imrasya <rasyadeans@gmail.com>
2022-12-23 08:34:05 +0700

    first commit

---
+++ data/Projects.js
@@ -0,0 +1,32 @@
+export const Projects = [
+  {
+    id: 1,
+    name: 'Simple Qr Code Generator',
+    slug: 'simpleqr',
+    img: 'simpleqr',
+    demo: 'http://qr.deanry.my.id/',
+    sourceCode: 'https://github.com/imrasya/simpleqrcode',
+    description:
+      'Simple Qr Code Generator With Api, Source Code Original (coding nepal)',
+  },
+  {
+    id: 2,
+    name: 'WhatsApp Bot',
+    slug: 'Wa Bot',
+    img: 'wabot',
+    demo: 'https://api.whatsapp.com/send?phone=+62812-3326-9919&text=.menu',
+    sourceCode: 'https://github.com/imrasya/RsyBot',
+    description:
+      'WhatsApp Bot With Node Js, Use Baileys (Md) Multi Device. This Bot Can Download YouTube Or Tiktok, And there are RPG games',
+  },
+  {
+    id: 3,
+    name: 'Coming Soon Project',
+    slug: 'coming-soon-project',
+    img: 'coming-soon',
+    demo: 'https://',
+    sourceCode: 'https://',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.',
+  },
+];
---
+++ pages/about/index.js
@@ -0,0 +1,37 @@
+import Layouts from '@/components/Layouts';
+import { Skills } from '@/data/Skills';
+import { motion } from 'framer-motion';
+import { ContentAnimation, FadeAnimation } from '@/components/Animations';
+
+const About = () => {
+  return (
+    <Layouts pageTitle=" | About">
+      <section className="relative flex w-full flex-col items-center justify-evenly py-40 md:h-screen md:flex-row md:overflow-hidden">
+        <motion.span {...FadeAnimation} className="title-page">
+          ABOUT
+        </motion.span>
+
+        <motion.div {...ContentAnimation} className="relative order-2 flex w-3/4 flex-col items-end justify-end md:order-1 md:w-1/3">
+          <div className="my-5 mr-2 text-left font-semibold text-primary-light">My Skills</div>
+          <div className="flex w-4/5 flex-wrap justify-evenly">
+            {Skills.map((skill) => (
+              <div key={skill.id} className="skills-icon">
+                <svg className="p-2" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d={skill.svg} />
+                </svg>
+              </div>
+            ))}
+          </div>
+        </motion.div>
+
+        <motion.div {...ContentAnimation} className="relative order-1 my-10 w-3/4 text-justify md:order-2 md:my-0 md:w-1/4">
+          <p className="text-md font-light text-primary-light">
+            {`Hi, my name is Rasya. I'm Developer of Rsy - Bot WhatsApp. I'm love coding because it's fun, writing lines of code to produce something. I'm like about computers since 2 years ago. I live in Surabaya, Indonesia. I was born on July 10, 2007, yeah I'm 15 years old. I'm like listening to music, especially slow or sad genres, i also like cats :3`}
+          </p>
+        </motion.div>
+      </section>
+    </Layouts>
+  );
+};
+
+export default About;
